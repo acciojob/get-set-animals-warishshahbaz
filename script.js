64a2724e889fdce57fb7673412219130f282c6f4
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-14 00:12:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 class Animal {
-	constructor(species){
-		this._species = species;
-	}
-	get species(){
-		return this._species;
-	}
+    constructor(species) {
+        this._species = species;
+    }
 
-	makeSound(){
-		console.log("The " + this.species + " make a sound")
-	}
+    get species() {
+        return this._species;
+    }
+
+    makeSound() {
+        console.log(`The ${this._species} makes a sound`);
+    }
 }
+
 class Cat extends Animal {
-	purr(){
-		console.log("purr")
-	}
+    purr() {
+        console.log("purr");
+    }
 }
+
 class Dog extends Animal {
-	bark(){
-		console.log("woof")
-	}
+    bark() {
+        console.log("woof");
+    }
 }
 
 
